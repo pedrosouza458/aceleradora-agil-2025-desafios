ac0f38139ef00279eb1a2e84f3800affe4f6676f
Pedro Souza <pedro.ads510@gmail.com>
2025-01-04 01:18:54 -0300

    feat: Modularize main message into a function and relocate to a new file

---
+++ loja-agilstore/src/functions/show-main-menu.ts
@@ -0,0 +1,11 @@
+export function showMainMenu() {
+  console.log(
+    `Bem-vindo à Loja AgilStore! Digite números para executar funções no nosso sistema: \n 
+     1 para Adicionar produto \n 
+     2 para Listar Produtos \n 
+     3 para Atualizar Produto \n 
+     4 para Excluir Produto \n 
+     5 para Buscar Produto \n
+     0 para Sair.`
+  );
+}
