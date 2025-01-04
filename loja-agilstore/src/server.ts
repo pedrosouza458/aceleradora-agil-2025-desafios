8728d8f51024b1297c56e6a2acc31ef07f258af2
Pedro Souza <pedro.ads510@gmail.com>
2025-01-04 01:14:56 -0300

    feat: Setting switch case base structure for user input

--- loja-agilstore/src/server.ts
+++ loja-agilstore/src/server.ts
@@ -1,1 +1,39 @@
-console.log("Loja Agilstore")
+#!/user/bin/env node
+import { showMainMenu } from "./functions/show-main-menu";
+
+showMainMenu()
+
+process.stdin.on("data", (data) => {
+  const input = data.toString().trim();
+
+  switch (input) {
+    case "1":
+      console.log("Adicionar produto");
+      break;
+
+    case "2":
+      console.log("Listar Produto");
+      break;
+
+    case "3":
+      console.log("Atualizar Produto");
+      break;
+
+    case "4":
+      console.log("Excluir Produto");
+      break;
+
+    case "5":
+      console.log("Buscar Produto");
+      break;
+
+    case "0":
+      console.log("Até logo");
+      process.exit();
+
+    default:
+      console.log("Opção inválida! Por favor, digite um número de 0 a 5.");
+      break;
+  }
+
+});
