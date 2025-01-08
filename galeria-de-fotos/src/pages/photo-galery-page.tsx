13831ff9cda12260d4cdc1c21b433d47183505a8
Pedro Souza <pedro.ads510@gmail.com>
2025-01-08 01:57:22 -0300

    feat: implement state logic for pagination

--- galeria-de-fotos/src/pages/photo-galery-page.tsx
+++ galeria-de-fotos/src/pages/photo-galery-page.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/header";
 import { PhotoGallery } from "../components/photo-gallery";
 import { SearchBar } from "../components/search-bar";
+import { Pagination } from "../components/pagination";
 
 export function PhotoGalleryPage() {
+  const [query, setQuery] = useState("beautiful nature");
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setPage(1);
+  }, [query]);
+
   return (
     <div>
       <Header />
-      <SearchBar />
-      <PhotoGallery />
+      <SearchBar query={query} setQuery={setQuery}  />
+      <Pagination page={page} setPage={setPage} />
+      <PhotoGallery query={query} page={page} />
     </div>
   );
 }
