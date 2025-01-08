7400a2639583b21b70c0558de1235b95c23cfa98
Pedro Souza <pedro.ads510@gmail.com>
2025-01-07 21:54:10 -0300

    chore: add constants

---
+++ galeria-de-fotos/src/api/constants.ts
@@ -0,0 +1,5 @@
+/* NOTE: The following API key is included for evaluation purposes only.
+ Do not use this key in a production environment. ( Message for git security check ) */
+export const UNSPLASH_API_KEY =
+  "Nm7UtxLLBXKZm617pbP3TnVO9luAFXUr2dY0sAdb408";
+export const UNSPLASH_BASE_URL = "https://api.unsplash.com/search/photos";
