8f5b35fd077d1b0d540b95d86b50c25d6a938a35
Pedro Souza <pedro.ads510@gmail.com>
2025-01-08 00:35:42 -0300

    feat: add footer component

---
+++ galeria-de-fotos/src/components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="text-center text-xs md:text-base lg:text-base left-0 bottom-0 py-3">
+      © 2024 Galeria de Fotos. Todos os direitos reservados. Feito com ❤️.
+    </footer>
+  );
+}
