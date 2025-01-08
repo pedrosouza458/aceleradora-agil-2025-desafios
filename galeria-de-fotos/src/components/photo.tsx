6d04bca82984cc41bfd82d9544a4e9e875a06512
Pedro Souza <pedro.ads510@gmail.com>
2025-01-08 00:36:56 -0300

    feat: add more fields to photo component

--- galeria-de-fotos/src/components/photo.tsx
+++ galeria-de-fotos/src/components/photo.tsx
@@ -1,21 +1,43 @@
 import { sanitizeUnsplashSlug } from "../utils/sanitize-unsplash-slug";
 
 interface PictureProps {
-  url: string;
+  image_url: string;
   name: string;
+  author: string;
+  author_photo: string;
+  likes: number;
 }
 
-export function Photo({ url, name }: PictureProps) {
+export function Photo({
+  image_url,
+  name,
+  author,
+  author_photo,
+  likes,
+}: PictureProps) {
   return (
-    <div>
+    <div className="px-3">
       <div className="relative overflow-hidden">
-      <img
-        className="h-60 w-full transform transition-transform duration-300 ease-in-out hover:scale-125"
-        src={url}
-        alt={name}
-      />
+        <img
+          className="h-60 object-cover w-full transform transition-transform duration-300 ease-in-out hover:scale-125"
+          src={image_url}
+          alt={name}
+        />
       </div>
-      <h1 className="text-center font-medium text-lg">{sanitizeUnsplashSlug(name)}</h1>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center space-x-4 py-2">
+          <img className="rounded-full h-8 " src={author_photo} alt="" />
+          <div className="flex flex-col justify-center ">
+            <p className="text-base font-medium">{author}</p>
+            <p className="text-sm font-medium text-slate-600">Autor</p>
+          </div>
+        </div>
+        <div className="flex items-center">{likes} ❤️</div>
+      </div>
+
+      <h1 className="text-left font-normal text-lg">
+        {sanitizeUnsplashSlug(name)}
+      </h1>
     </div>
   );
 }
