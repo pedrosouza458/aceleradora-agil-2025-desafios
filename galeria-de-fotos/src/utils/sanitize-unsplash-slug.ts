7660e95211838cc344eaf6efc8ae2df64e7b2456
Pedro Souza <pedro.ads510@gmail.com>
2025-01-08 00:35:23 -0300

    fix: improve sanitizeUnsplashSlug

--- galeria-de-fotos/src/utils/sanitize-unsplash-slug.ts
+++ galeria-de-fotos/src/utils/sanitize-unsplash-slug.ts
@@ -1,7 +1,9 @@
 export function sanitizeUnsplashSlug(slug: string): string {
+  
   const parts = slug.split('-');
-  parts.pop(); 
-  const sanitized = parts
+  const sanitizedParts = parts.filter(part => /^[a-zA-Z]+$/.test(part));
+  const sanitized = sanitizedParts
+
     .map((word, index) => {
       if (index === 0) {
         return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
